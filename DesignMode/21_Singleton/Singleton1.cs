3953e2d2354b263f8cb764689b39a904d1ce40f2
a673544319 <673544319@qq.com>
2017-05-16 11:55:26 +0800

    单例模式

---
+++ DesignMode/21_Singleton/Singleton1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignMode._21_Singleton {
+    /// <summary>
+    /// 饿汉单例模式
+    /// </summary>
+    class Singleton1 {
+        private static readonly Singleton1 instance=new Singleton1();
+
+        public static Singleton1 Instance {
+            get { return instance; }
+        }
+
+        private Singleton1() { }
+    }
+}
---
+++ DesignMode/21_Singleton/Singleton2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignMode._21_Singleton {
+    // 懒汉单例模式
+    class Singleton2 {
+        private static Singleton2 instance;
+        private static Object lockO = new Object();
+
+        public static Singleton2 Instance {
+            get {
+                // 双重锁定
+                if (instance == null) {
+                    lock (lockO) {
+                        if (instance == null) {
+                            instance = new Singleton2();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        private Singleton2() { }
+    }
+}
